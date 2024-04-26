9fb181399468275dd5fc7e3ae915ba120532963e
Filipp <filipka790@mail.ru>
2024-04-26 19:23:02 +0300

    ЗАКОНЧИЛ РАБОТ

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,12 @@
 
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
         AccountDivider AccountDivider = new AccountDivider();
         AccountDivider.start();
+
+
 
     }
 }
